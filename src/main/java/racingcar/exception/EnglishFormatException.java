a7c9ec846adc758457d8eab2efbdad6d7bbdadde
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-08-22 15:18:38 +0900

    feat: 영어 포맷 커스텀 예외 추가

---
+++ src/main/java/racingcar/exception/EnglishFormatException.java
@@ -0,0 +1,8 @@
+package racingcar.exception;
+
+public class EnglishFormatException extends IllegalArgumentException {
+
+    public EnglishFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
