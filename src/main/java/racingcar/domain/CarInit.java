fd460e7be5832b0880ae41938344859299452669
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-08-25 15:44:44 +0900

    feat: 자동차 입력 클래스 생성

---
+++ src/main/java/racingcar/domain/CarInit.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class CarInit {
+
+    public String[] inputCars() {
+        return readLine().split(",");
+    }
+
+    public int inputTry() {
+        return Integer.parseInt(readLine());
+    }
+}
